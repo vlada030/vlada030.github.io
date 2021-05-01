25f8476aba0d0a5ccccf41126249a9a236cac83f
Vlada DELL <vlada030@gmail.com>
2021-05-01 21:25:19 +0200

    izmena sajta - implementacija Swipera

--- resources/js/year.js
+++ resources/js/year.js
@@ -1,3 +1,26 @@
 window.addEventListener('load', () => {
     document.querySelector('footer #year').innerText = `${new Date(Date.now()).getFullYear()}`;
 })
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  });
