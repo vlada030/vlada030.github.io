9e85adbc6bcdb90383de45137e78cbbb85353057
Vlada DELL <vlada030@gmail.com>
2021-05-04 20:23:19 +0200

    preradjena strana, dodati novi projekti

--- resources/js/year.js
+++ resources/js/year.js
@@ -7,6 +7,7 @@ const swiper = new Swiper('.swiper-container', {
   spaceBetween: 30,
   centeredSlides: true,
   loop: true,
+  // effect: 'fade',
   // autoplay: {
   //   delay: 2500,
   //   disableOnInteraction: false,
